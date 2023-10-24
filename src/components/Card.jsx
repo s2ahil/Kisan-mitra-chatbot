21d279699826a448dcabb4fc17598909d2c91a91
s2ahil <101473078+s2ahil@users.noreply.github.com>
2023-10-24 15:33:20 +0530

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Card = ({ name, desc, image, tag1, tag2, btnLink }) => {
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <img className="w-full" src={image} alt={name} />
+      <img className="w-full h-[10rem]" src={image} alt={name} />
       <div className="px-6 py-4">
         <div className="font-bold text-xl mb-2">{name}</div>
         <p className="text-gray-700 text-base">{desc}</p>
